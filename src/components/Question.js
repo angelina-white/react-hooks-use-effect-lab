25ec2f6e79dc6e8c1b35ac8848ae72063892c2c7
Angelina <angelinarwhite@gmail.com>
2022-05-23 13:15:22 -0400

    make timer work

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -9,6 +10,25 @@ function Question({ question, onAnswered }) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+  useEffect(() => 
+  {
+    let myInterval = setInterval(() => 
+    {
+      if (timeRemaining > 0) 
+      {
+          setTimeRemaining(timeRemaining - 1);
+      }
+      else
+      {
+        {onAnswered(false)}
+        setTimeRemaining(10)
+      }
+    }, 1000)
+  return ()=> {
+      clearInterval(myInterval);
+    };
+});
 
   const { id, prompt, answers, correctIndex } = question;
 
